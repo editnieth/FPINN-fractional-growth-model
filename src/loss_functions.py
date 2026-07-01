b9a9700674e4aaed8f27fa446354c4772a38c066
editnieth <26500559@uagro.mx>
2026-06-30 18:15:16 -0600

    Implement equation_loss and data_loss functions
    Added loss functions for inverse problem using TensorFlow.

--- src/loss_functions.py
+++ src/loss_functions.py
@@ -1,1 +1,62 @@
 # Loss functions for inverse problem
+import tensorflow as tf
+
+
+
+def equation_loss(solution, caputo_derivative, a):
+    """
+    Loss function associated with the fractional differential equation.
+
+    Measures the residual:
+
+        D^alpha Y(t) = a Y(t)
+
+    Parameters
+    ----------
+    solution : tf.Tensor
+        Neural network approximation of the solution.
+
+    caputo_derivative : tf.Tensor
+        Approximation of the Caputo fractional derivative.
+
+    a : tf.Tensor
+        Growth parameter.
+
+    Returns
+    -------
+    tf.Tensor
+        Mean squared residual of the differential equation.
+    """
+
+    residual = caputo_derivative - (a * solution)
+
+    return tf.reduce_mean(
+        tf.square(residual)
+    )
+
+
+
+
+def data_loss(solution, observations):
+    """
+    Loss function associated with experimental data.
+
+    Parameters
+    ----------
+    solution : tf.Tensor
+        Neural network prediction.
+
+    observations : tf.Tensor
+        Experimental data.
+
+    Returns
+    -------
+    tf.Tensor
+        Mean squared error between prediction and data.
+    """
+
+    error = solution - observations
+
+    return tf.reduce_mean(
+        tf.square(error)
+    )
